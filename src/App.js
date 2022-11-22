e58ef153041077f2cf5e73c56a1a3c8ba84c3d5a
Erdij <yildirimsukruerdi@gmail.com>
2022-11-22 23:15:07 +0300

    apiKey

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import { usePosition } from "use-position";
 import axios from "axios";
@@ -9,7 +9,18 @@ function App() {
   console.log(latitude);
   console.log(longitude);
 
-  const getWeatherData = async (lat, lon) => {};
+  const getWeatherData = async (lat, lon) => {
+    const key = process.env.REACT_APP_WEATHER_DATA;
+    console.log(key);
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`
+      );
+      console.log(data);
+    } catch (error) {
+      alert("!!!!!");
+    }
+  };
 
   useEffect(() => {
     latitude && longitude && getWeatherData(latitude, longitude);
